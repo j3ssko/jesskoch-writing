e9b4fd8d2b0ee8b00eb68e119243f76041259347
Jess Koch <jkoch@jesss-mbp.home>
2020-12-03 19:04:29 -0500

    reconfig server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 app.use(express.static("express"));
 // default URL for website
-app.use('/', function(req,res){
+app.use('https://jesskoch-writing.herokuapp.com/', function(req,res){
     res.sendFile(path.join(__dirname+'/express/index.html'));
     //__dirname : It will resolve to your project folder.
   });
